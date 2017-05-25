4f835983e65b0e0c4dffd64bd3cfee2531315f4b
Christian <christianbrintnall@gmail.com>
2017-05-25 12:03:54 -0700

    Display hours from DB

--- views.py
+++ views.py
@@ -1,29 +1,35 @@
 '''Hour manager sub directory views stored here'''
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from hour_manager.models import HourModel
 
-from .models import HourModel
 from .forms import HourAddForm
 
 def index(request):
     '''Return the index page.'''
 
-    hours = "Hello!"
+    #Some linters may say this line below is invalid, it IS valid
+    hours = HourModel.objects.all()
+
+    context = {
+        "hours":hours,
+    }
 
     return render(
         request,
         'index.html',
-        context=None
+        context
     )
 
 @login_required
 def AddHour(request):
+    '''Page to add hours to the hour manager'''
     form = HourAddForm(request.POST or None)
 
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
-        print (instance.first_name)
+        print(instance.first_name)
 
     context = {
         "form": form
