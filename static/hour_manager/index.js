a459d01cac1d2240ccc7ace52140ce458b0226a4
Christian <christianbrintnall@gmail.com>
2017-07-10 13:21:17 -0800

    Fix menu css, clean up main menu css.

--- static/hour_manager/index.js
+++ static/hour_manager/index.js
@@ -1,19 +1,12 @@
 $(document).ready(function() {
-    $(".menu").animate({width:"200px"});
-    $(".menu a").delay(800).animate({opacity:"1"});
-    $(".reasontd").hide();
-});
-
-$(document).ready(function() {
     $("tr").mouseover(function() {
         $(this).find('.reasontd').css({
-            "z-index":"1",
-            "position":"relative",
-            "overflow-x":"visible",
-            "overflow-y":"hidden",
-            "color":"white",
+            "opacity":"1",
         });
-    }).mouseout(function() {
-        $(this).find('.reasontd').hide()
+    })
+    $("tr").mouseout(function() {
+        $(this).find('.reasontd').css({
+            "opacity":"0"
+        });
     })
 });
---
+++ static/hour_manager/menu.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $(".menu").animate({width:"200px"});
+    $(".menu a").delay(800).animate({opacity:"1"});
+});
