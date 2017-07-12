fa1cf388e92a41577ebcd3d65236b67fbb3d21cf
Christian <christianbrintnall@gmail.com>
2017-07-12 14:25:27 -0800

    Add form validation via backend

--- forms.py
+++ forms.py
@@ -2,6 +2,8 @@ from django import forms
 
 from .models import HourModel
 
+from datetime import datetime
+
 class HourAddForm(forms.ModelForm):
     class Meta:
         model = HourModel
@@ -11,3 +13,23 @@ class HourAddForm(forms.ModelForm):
             "end_time",
             "reason",
         ]
+
+    def clean(self):
+        super(HourAddForm, self).clean()
+        date = self.cleaned_data.get('date', False)
+        start_time = self.cleaned_data.get('start_time', False)
+        end_time = self.cleaned_data.get('end_time', False)
+
+        date_obj = datetime.now()
+        current_time = date_obj.time()
+        current_date = date_obj.date()
+
+        if date < current_date:
+            raise forms.ValidationError(
+                    "Please enter a day after today's date"
+                )
+
+        if end_time < start_time:
+            raise forms.ValidationError(
+                    "Please make sure your end time is after your start time"
+                )
