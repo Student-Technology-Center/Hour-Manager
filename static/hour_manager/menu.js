0211f8abaa6eb57b6fdaf0fa080a497fef30f219
Christian <christianbrintnall@gmail.com>
2017-07-12 12:07:39 -0800

    Make slide in only happen once per session

--- static/hour_manager/menu.js
+++ static/hour_manager/menu.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
-    $(".menu").animate({width:"200px"});
-    $(".menu a").delay(800).animate({opacity:"1"});
+    if (sessionStorage.getItem('menu-state') !== 'open') {
+        $(".menu").animate({width:"200px"});
+        $(".menu a").delay(800).animate({opacity:"1"});
+        sessionStorage.setItem('menu-state','open');
+    } else {
+        $(".menu").css({"transition":"0s","width":"200px"});
+        $(".menu a").css({"transition":"0s","opacity":"1"});
+    }
 });
