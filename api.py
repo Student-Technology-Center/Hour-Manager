8d8989ff34f3951f6ad711c63d02a6000533ef18
Christian <christianbrintnall@gmail.com>
2017-07-14 11:05:58 -0800

    Live update front page with new times

--- api.py
+++ api.py
@@ -4,17 +4,39 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, JsonResponse
 from hour_manager.models import HourModel, hour_history
 
+import json
+
 @login_required
 def hour(request, pk):
     hour = HourModel.objects.get(pk=pk)
+    print(hour.start_time.strftime('%I:%M %p') if hour.start_time.strftime('%I:%M %p')[0] != "0" else hour.start_time.strftime('%I:%M %p')[1:])
     return JsonResponse(
         {
+            "pk":hour.pk,
             "username":hour.username,
             "first_name":hour.first_name,
             "last_name":hour.last_name,
-            "date":hour.date.isoformat(),
-            "start_time":hour.start_time.strftime('%H:%M'),
-            "end_time":hour.end_time.strftime('%H:%M'),
+            "date":hour.date.strftime('%B %d, %Y'),
+            "start_time":hour.start_time.strftime('%I:%M %p') if hour.start_time.strftime('%I:%M %p')[0] != "0" else hour.start_time.strftime('%I:%M %p')[1:],
+            "end_time":hour.end_time.strftime('%I:%M %p') if hour.end_time.strftime('%I:%M %p')[0] != "0" else hour.end_time.strftime('%I:%M %p')[1:],
             "reason":hour.reason,
         }
     )
+
+@login_required
+def all_hours(request):
+    hours = HourModel.objects.all()
+    json_hours = [
+            {
+                "username":hour.username,
+                "first_name":hour.first_name,
+                "last_name":hour.last_name,
+                "date":hour.date.strftime('%B %d, %Y') ,
+                "start_time":hour.start_time.strftime('%I:%M %p') if hour.start_time.strftime('%I:%M %p')[0] != "0" else hour.start_time.strftime('%I:%M %p')[1:],
+                "end_time":hour.end_time.strftime('%I:%M %p') if hour.end_time.strftime('%I:%M %p')[0] != "0" else hour.end_time.strftime('%I:%M %p')[1:],
+                "reason":hour.reason,
+            }
+            for hour in hours
+        ]
+    
+    return JsonResponse(json_hours, safe=False)
