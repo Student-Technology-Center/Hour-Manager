d14f7f0ca6fa6a62f89a24a59e4965cb68b10f9c
STC <STC@hhd191-112.atus.wwu.edu>
2017-10-10 13:48:53 -0700

    Fix incorrect time values

--- static/hour_manager/js/index.js
+++ static/hour_manager/js/index.js
@@ -2,11 +2,27 @@ var claimLink;
 var selectedTr;
 
 function standardToMilitary(time) {
-    var num = time.slice(0, 1);
-    var newNum = parseInt(num);
-    newNum += 12;
-    var num = newNum.toString();
-    num += ":00";
+    if (time == "noon") {
+        return "12:00";
+    }
+
+    console.log(time);
+
+    var num;
+
+    if (num != '0') {
+        num = time.slice(0, 2);
+
+        if (num.slice(1, 2) == ' '){
+            var newNum = parseInt(num);
+            newNum += 12;
+            var num = newNum.toString();
+            num += ":00";
+        } else {
+            num += ":00";
+            return num;
+        }
+    }
 
     return num;
 }
