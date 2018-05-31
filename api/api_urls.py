949eec11c16e587bb76f560362a9c2958f6b5953
Christian <christianbrintnall@gmail.com>
2018-05-30 20:51:41 -0700

    Add class based hour endpoint

--- api/api_urls.py
+++ api/api_urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
-from hour_manager.api.api import hour
+from hour_manager.api.api import Hour
 
 urlpatterns = [
-    url(r'^hour/(?P<pk>[0-9])*/?$', hour, name='hour')
+    url(r'^hour/(?P<pk>[0-9])*/?$', Hour.as_view(), name='hour')
 ]
